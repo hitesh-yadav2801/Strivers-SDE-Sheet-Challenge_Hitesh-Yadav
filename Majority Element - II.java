af5802ee3790691885401a6f8d7b9268f9df108e
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-19 22:28:58 +0530

    Update Majority Element - II.java

--- Majority Element - II.java
+++ Majority Element - II.java
@@ -23,3 +23,49 @@ public class Solution
         return ans;
     }
 }
+
+
+// Optimal Solution without extra space
+
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static ArrayList<Integer> majorityElementII(ArrayList<Integer> arr) 
+    {
+       int n = arr.size();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if(cnt1 == 0 && ele2 != arr.get(i)){
+                cnt1 = 1;
+                ele1 = arr.get(i);
+            }else if(cnt2 == 0 && ele1 != arr.get(i)){
+                cnt2 = 1;
+                ele2 = arr.get(i);
+            } else if(arr.get(i) == ele1){
+                cnt1++;
+            } else if(arr.get(i) == ele2){
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if(ele1 == arr.get(i)) cnt1++;
+            if(ele2 == arr.get(i)) cnt2++;
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(cnt1 > n / 3) ans.add(ele1);
+        if(cnt2 > n / 3) ans.add(ele2);
+        return ans;
+    }
+}
