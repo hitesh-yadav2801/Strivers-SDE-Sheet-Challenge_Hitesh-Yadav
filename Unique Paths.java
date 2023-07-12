551b44564f3531a3408a06261a9aa54a16bc8b84
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-12 09:09:04 +0530

    Update Unique Paths.java

--- Unique Paths.java
+++ Unique Paths.java
@@ -1,3 +1,6 @@
+// Recursion + memo
+
+
 import java.util.* ;
 import java.io.*; 
 public class Solution {
@@ -23,5 +26,28 @@ public class Solution {
 		int path2 = path(s1 + 1, s2, dp);
 
 		return dp[s1][s2] = path1 + path2;
+	}
+}
+
+// Tabulation
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int uniquePaths(int m, int n) {
+		int dp[][] = new int[m][n];
+		
+		for(int i = 0; i < m; i++){
+			dp[i][n - 1] = 1;
+		}
+		for(int i = 0; i < n; i++){
+			dp[m - 1][i] = 1;
+		}
+		for(int i = m - 2; i >= 0; i--){
+			for(int j = n - 2; j >= 0; j--){
+				dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+			}
+		}
+		return dp[0][0];
 	}
 }
