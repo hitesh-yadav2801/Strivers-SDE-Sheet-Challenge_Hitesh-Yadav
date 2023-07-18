f743a3e2e15f92ac979fad414ff06978b15f6633
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-18 21:57:44 +0530

    Update Trapping Rain Water.java

--- Trapping Rain Water.java
+++ Trapping Rain Water.java
@@ -30,3 +30,27 @@ public class Solution {
         return trappedWater;
     }
 }
+
+// o(1) space and o(n) time
+
+public class Solution {
+    public static long getTrappedWater(long[] arr, int n) {
+        long leftMax = 0;
+        long rightMax = 0;
+        long water = 0;
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            if (arr[l] <= arr[r]) {
+                if (arr[l] >= leftMax) leftMax = arr[l];
+                else water += leftMax - arr[l];
+                l++;
+            } else {
+                if (arr[r] >= rightMax) rightMax = arr[r];
+                else water += rightMax - arr[r];
+                r--;
+            }
+        }
+        return water;
+    }
+}
+
