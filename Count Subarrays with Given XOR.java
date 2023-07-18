ad0fa6a97c942ed79c1844a5440aeeb01ee47971
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-18 23:28:41 +0530

    Update Count Subarrays with Given XOR.java

--- Count Subarrays with Given XOR.java
+++ Count Subarrays with Given XOR.java
@@ -19,3 +19,30 @@ public class Solution {
 		return ans;
 	}
 }
+
+
+// Optimal solution (O(n) TC & SC)
+
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public static int subarraysXor(ArrayList<Integer> arr, int x) {
+		int n = arr.size();
+		int ans = 0;
+		int xr = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		for(int i = 0; i < n; i++){
+			xr ^= arr.get(i);
+			int k = xr ^ x;
+			if(map.containsKey(k)){
+				ans += map.get(k);
+			} 
+			map.put(xr, map.getOrDefault(xr, 0) + 1);
+		}
+		return ans;
+	}
+}
